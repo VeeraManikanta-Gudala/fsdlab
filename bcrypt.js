5048d7ac0f52a703c66d92dfbe9b8396412bf1e6
VeeraManikanta-Gudala <gveera_it211229@mgit.ac.in>
2024-09-02 19:51:01 +0530

    everything alright

--- bcrypt.js
+++ bcrypt.js
@@ -1,46 +1,89 @@
 var bcrypt = require('bcryptjs');
+
 var readlineSync = require('readline-sync');
-var valids = require('./valid.js')
+
+var valids = require('./valid.js');
+
 const fs = require('fs');
-function login(){
+
+function login() {
+
     var Username = readlineSync.question('Enter username:');
 
-}
-function signup(){
-    var name = readlineSync.question("Enter Username:")
+    var enteredPassword = readlineSync.question('Enter password: ', { hideEchoBack: true });
 
-    var res = validUsername(name);
-while(!res){
-    let res1=validUsername(name)
-    if(res1){
-        break;
+    const storedUsernames = fs.readFileSync('usernames.txt', 'utf-8').split('\n');
+
+    const storedPasswords = fs.readFileSync('Passwords.txt', 'utf-8').split('\n');
+
+    const userIndex = storedUsernames.indexOf(Username);
+
+    if (userIndex !== -1) {
+        const storedHashedPassword = storedPasswords[userIndex];
+        const passwordMatch = bcrypt.compareSync(enteredPassword, storedHashedPassword);
+
+        if (passwordMatch) {
+            console.log('Login successful!');
+        } else {
+            console.log('Invalid password.');
+        }
+    } else {
+        console.log('Username not found.');
     }
 }
-fs.appendFileSync('usernames.txt',name);
-var email = readlineSync.question("Enter Email:")
-var res2 = validEmail(email);
-while(!res2){
-    let res1=validEmail(email)
-    if(res1){
-        break;
+
+function signup() {
+    var name = readlineSync.question('Enter Username:');
+
+    var res = valids.validUsername(name);
+
+    while (!res) {
+
+        name = readlineSync.question('Enter Username:');
+
+        res = valids.validUsername(name);
     }
-}
-fs.appendFileSync('emails.txt',email);
-var pass = readlineSync.question("Enter Password:")
-var res3 = validPassword(pass);
-while(!res3){
-    let res1=validPassword(pass)
-    if(res1){
-        break;
+
+    const storedUsernames = fs.readFileSync('usernames.txt', 'utf-8').split('\n');
+
+    if (storedUsernames.includes(name)) {
+
+        console.log('Username already exists. Please choose another one.');
+
+        return;
     }
+
+    
+    var email = readlineSync.question('Enter Email:');
+    
+    var res2 = valids.validEmail(email);
+    
+    while (!res2) {
+        email = readlineSync.question('Enter Email:');
+        res2 = valids.validEmail(email);
+    }
+    
+    
+    var pass = readlineSync.question('Enter Password:', { hideEchoBack: true });
+    var res3 = valids.validPassword(pass);
+    while (!res3) {
+        pass = readlineSync.question('Enter Password:', { hideEchoBack: true });
+        res3 = valids.validPassword(pass);
+    }
+    
+    const hashedPassword = bcrypt.hashSync(pass, 10);
+    fs.appendFileSync('usernames.txt', name + '\n');
+    fs.appendFileSync('emails.txt', email + '\n');
+    fs.appendFileSync('Passwords.txt', hashedPassword + '\n');
+    
+    console.log('Signup successful!');
 }
-fs.appendFileSync('Passwords.txt',name);
-}
-var choice = readlineSync.question('Press (1) for login\n press (2) for signup');
-if(choice === 1){
+
+var choice = readlineSync.question('Press (1) for login\nPress (2) for signup\n');
+if (choice === '1') {
     login();
-}else{
+} else if (choice === '2') {
     signup();
+} else {
+    console.log('Invalid choice.');
 }
-
-
--- valid.js
+++ valid.js
@@ -1,28 +1,34 @@
-var readlineSync = require('readline-sync');
 function validUsername(name) {
  
     if(name.length<3 || name.length > 25){
+        console.log("Username length should be between 3 and 25");
+        
         return false;
     }
-    if(!/[a-zA-Z]/.test(name)){
+    if(!/[a-zA-Z]+$/.test(name)){
+        console.log("Username should only contain alphabets.");
+        
         return false;
     }
     return true;
 }
-function validEmail() {
-    var email = readlineSync.question("Enter Email:")
+function validEmail(email) {
     if(!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email)){
+        console.log("Invalid Email");
+        
         return false;
     }
     return true;
 }
-function validPassword(){
-    var Password = readlineSync.question("Enter Password:",{hideEchoBack:true})
+function validPassword(Password){
     if(!/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(Password)){
+        console.log("password must contain alphanumeri and special characters");
+        
         return false;
     }
     return true;
 }
+
 module.exports = {
     validUsername,validEmail,validPassword
 }
